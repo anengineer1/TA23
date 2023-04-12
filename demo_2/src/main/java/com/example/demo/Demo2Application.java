06d477a252d00897f2fbad77fb5171e0a7d1c253
Francisco <franparpe@disroot.org>
2023-04-12 16:08:49 +0200

    First commit demo_2

---
+++ demo_2/src/main/java/com/example/demo/Demo2Application.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Demo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo2Application.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "John Doe") String name) {
+		return String.format("Hello yo, %s!", name);
+	}
+
+	@GetMapping("/fire")
+	public String fire() {
+		return String.format("Fire in the hole!");
+	}
+	
+}
